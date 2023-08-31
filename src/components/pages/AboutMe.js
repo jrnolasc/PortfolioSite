3ffa1562a8c633d1bc26b8728fdd983eed22e178
jrnolasc <54826935+jrnolasc@users.noreply.github.com>
2023-08-30 21:42:48 -0500

    updates to about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -7,7 +7,6 @@ function AboutMe() {
     <>
       <div className="aboutMe-Container">
         <div className="leftSection">
-          <img className="abtMe-img" src="images/header_img.png" alt="idk" />
           <h1 className="abtMe">About Me</h1>
           <p className="summary">
             With over six years of experience in the tech industry, I am a
@@ -17,8 +16,58 @@ function AboutMe() {
             practices, and cutting-edge web development. Throughout my journey,
             I have honed my skills and refined my problem-solving abilities,
             always seeking to bridge the gap between creativity and technical
-            precision.
+            precision. I am also known for being a collaborative team player,
+            thriving in environments where I can work closely with others to
+            achieve shared goals.
           </p>
+          <div className="abt-facts">
+            <div className="abt-facts-box">
+              <ul>
+                <li>
+                  <i class="fa-solid fa-graduation-cap"></i>
+                  <p className="facts-text">
+                    Earned my Computer Science degree from the University of
+                    Arkansas.
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="abt-facts-box">
+              <ul>
+                <li>
+                  <i class="fa-solid fa-screwdriver-wrench"></i>
+                  <p className="facts-text">
+                    I enjoy restoring classic cars in my free time. Right now,
+                    I'm busy working on a 76 Firebird!
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="abt-facts-box">
+              <ul>
+                <li>
+                  <i class="fa-solid fa-music"></i>
+                  <p className="facts-text">
+                    I enjoy picking up new instruments. I've been playing the
+                    violin for over 7 years, and now I'm diving into learning
+                    the flute.
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="abt-facts-box">
+              <ul>
+                <li>
+                  <i class="fa-solid fa-headphones"></i>
+                  <p className="facts-text">
+                    I used to host an in-house podcast where I chatted with my
+                    colleagues. It was a great way to learn more about them and
+                    what drives them.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="rightSection">
           <div className="skill-box">
